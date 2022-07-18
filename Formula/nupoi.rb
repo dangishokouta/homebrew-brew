d7585526db14615d6ea442c55fdd797e4d5f5d71
dangishokouta <90143019+dangishokouta@users.noreply.github.com>
2022-07-18 16:22:04 +0900

    Update nupoi.rb

--- Formula/nupoi.rb
+++ Formula/nupoi.rb
@@ -2,32 +2,26 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Nupoi < Formula
-  desc ""
-  homepage ""
+  desc "Check for null in the dataset"
+  homepage "https://github.com/dangishokouta/nupoi"
   url "https://github.com/dangishokouta/nupoi/archive/refs/tags/v0.1.1.tar.gz"
+  version VERSION
   sha256 "f503227fa5db75d0cd155d78822a239e323c6de7cf7ad7633e9fe924080c2bf6"
-  license ""
+  license "MIT"
+
+  option "without-completions", "Disable bash completions"
+  depends_on "rustup" => :build
+  depends_on "bash-completion@2" => :optional
 
   # depends_on "cmake" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    system "./configure", *std_configure_args, "--disable-silent-rules"
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
-  end
+    bin.install "nupoi"
+    prefix.install "README.md"
+    prefix.install "LICENSE"
 
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test nupoi`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    bash_completion.install "completions/bash/nupoi" if build.with? "completions"
+    zsh_completion.install  "completions/zsh/_nupoi" if build.with? "completions"
+    fish_completion.install "completions/fish/nupoi" if build.with? "completions"
   end
 end
